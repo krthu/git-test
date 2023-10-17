c22325ce69aa2c9004041c0572f0f008d79bcbc6
Ina Burström <inaburstrom@hotmail.com>
2023-10-17 11:00:02 +0200

    calculation of x and y

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         // Här skriver vi ut något på skärmen.
-        
+
+        int x = 3;
+        int y = 7;
+        int summa = x + y;
+
+        System.out.println("Summan av " + x + " och " + y + " är " + summa);
     }
 }
